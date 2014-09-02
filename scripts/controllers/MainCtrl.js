0c4a86849f44a86cb366571a38f03b4f94bd1b77
nmusgrave <naomi.g.musgrave@gmail.com>
2014-09-01 23:12:26 -0700

    added pics to front page

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -1,5 +1,15 @@
 // create the controller and inject Angular's $scope
 personalSiteApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Welcome';
+  // create a message to display in our view
+  $scope.message = 'Welcome';
+  
+  $scope.dir = 'images';
+  $scope.horizontal = [ 'gorgeB&W.jpg','IMG_4029.JPG' ];
+  $scope.vertical = [ 'road.jpg', 'trees.jpg', 'IMG_3722.JPG', 'IMG_4027.JPG' ];
+
+  var init = function() {
+    
+  };
+  
+  init();
 });
