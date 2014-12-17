05982087c3366536232fe8388340105975088c02
nmusgrave <naomi.g.musgrave@gmail.com>
2014-12-16 22:54:55 -0800

    latex added

--- scripts/controllers/CaffeineCtrl.js
+++ scripts/controllers/CaffeineCtrl.js
@@ -8,70 +8,83 @@
  * Controller of the ngPersonalSiteApp
  */
 personalSiteApp.controller('caffeineController', function($scope) {
-    $scope.message = "hello";
+
+    // Public Vars ------
+    // hrs
     $scope.time = 0;
-    $scope.data = [
-    {
-        "key": "Caffeine Level",
-        "values": []
-    },
-    {
-        "key": "Exogenous Circadian",
-        "values": []
-    },
-    {
-        "key": "Endogenous Circadian",
-        "values": []
-    },
-    {
-        "key": "Total Circadian",
-        "values": []
-    },
-    {
-        "key": "Scaled Base",
-        "values": []
-    },
-    {
-        "key": "Base Caffeine Level",
-        "values": []
-    }];
-    
+    // mgs
+    $scope.minCaffeine = 0;
+    $scope.curCaffeine = 0;
+    $scope.nextCaffeine = 0;
+    // max total consumption for one day
+    $scope.maxCaffeine = 0;
+    // hrs
+    $scope.doseTime = 0;
+
+    // Private Vars ------
     var absorbK = 11.76;
     var elimK = 0.162;
-    var exoCircadian = [];
-    var endoCircadian = [];
-    var totalCircadian = [];
-    var targetCaffeine = [];
-    var baseCaffeine = [];
     
     /*
-     * initial: starting caffeine intake
-     * target: ending caffeine intake
-     * returns time till reaching that target caffeine level
-     * units are in hours and milligrams
+     * initial: starting caffeine amount (mgs)
+     * target: optimimum caffeine intake (mgs)
+     * returns time (hrs) till reaching that target caffeine level
      */
-    var getTime = function(initial, target) {
-      var t = Math.log(target * (absorbK - elimK) / (absorbK * initial) ) /
+    var getTime = function(initial) {
+      return Math.log($scope.minCaffeine * (absorbK - elimK) / (absorbK * initial) ) /
         Math.log(Math.pow(Math.E, -elimK) - Math.pow(Math.E, -absorbK));
-      return t;
     }
     
+    /*
+     * initial: starting caffeine amount (mgs)
+     * t: caffeine amount at some time in the future (hrs)
+     * returns ending caffeine amount (mgs)
+     */
     var getAmount = function(initial, t) {
-      var a = absorbK * initial * (Math.pow(Math.E, -elimK * t) - Math.pow(Math.E, -absorbK * t))
-        / (absorbK - elimK);
-      return a;
+      return absorbK * initial * (Math.pow(Math.E, -elimK * t) - Math.pow(Math.E, -absorbK * t)) / (absorbK - elimK);
     }
     
-    // t is time of day
+    /*
+     * t: time of day
+     * returns: ???
+     */
     var getEndogenousCircadian = function(t) {
-      var c = (2.5 * Math.cos((2 * Math.PI)*(t - 16.8) / 24))*5;
-      return c;
+      return (2.5 * Math.cos((2 * Math.PI)*(t - 16.8) / 24))*5;
     }
     
-    // t is time of day
+    /*
+     * t: time of day
+     * returns: ???
+     */
     var getExogenousCircadian = function(t) {
-      var c = (-0.5 + 0.5 * Math.cos( (2 * Math.PI)*(t - 16.8 - 3)/12 ) ) + 5;
-      return c;
+      return (-0.5 + 0.5 * Math.cos( (2 * Math.PI)*(t - 16.8 - 3)/12 ) ) + 5;
+    }
+
+    /*
+     * lbs: weight (lbs)
+     * returns: mass (kgs)
+     */
+    var convertLbsToKgs = function(lbs) {
+      return lbs * 0.453592;
+    }
+
+    /*
+     * age (years)
+     * returns: hazardous daily consumption total (mgs)
+     */
+    var dangerousAmount = function(age) {
+      if(age < 4)
+        return 0;
+      else if(age <= 6)
+        return 45;
+      else if(age <= 9)
+        return 62.5;
+      else if(age <= 12)
+        return 85;
+      else if(age <= 18)
+        return 100;
+      else
+        return 400;
     }
     
     $scope.calculate = function() {
@@ -79,46 +92,24 @@ personalSiteApp.controller('caffeineController', function($scope) {
       var hrs = d.getHours();
       console.log(hrs + $scope.duration);
       var initial = 140; //mgs
-      var target = $scope.weight * 0.453592 * 1.5; //mgs
-      $scope.time = getTime(initial, target);
-      var amounts = [];
-      var currentCaf = initial;
-      for(var i = hrs; i <= $scope.duration + hrs; i += 0.5) {
-        currentCaf = getAmount(currentCaf, 1);        
-        if (currentCaf < target) {
-          currentCaf += 34;
-          console.log(34 + " time: " + i);
-        }
-        var index = i - hrs;
-        exoCircadian[index]   = [i, getExogenousCircadian(i + hrs)];
-        endoCircadian[index]  = [i, getEndogenousCircadian(i + hrs)];
-        var total = getExogenousCircadian(i + hrs) + getEndogenousCircadian(i + hrs);
-        var base =  target * total / 200 + initial - 50;
-        
-        currentCaf = getAmount(currentCaf, 1);        
-        if (currentCaf < base) {
-          currentCaf += 34;
-        }
-        
-        totalCircadian[index] = [i, getExogenousCircadian(i + hrs) + getEndogenousCircadian(i + hrs)];
-        targetCaffeine[index] = [i, base];
-        baseCaffeine[index]   = [i, target];
-        amounts[index]        = [i, currentCaf];
-      }
-      
-      $scope.data[0].values = amounts;
-      $scope.data[1].values = exoCircadian;
-      $scope.data[2].values = endoCircadian;
-      $scope.data[3].values = totalCircadian;
-      $scope.data[4].values = targetCaffeine;
-      $scope.data[5].values = baseCaffeine;
-      
-      $scope.showChart = true;
-      $scope.$apply();
+
+      $scope.maxCaffeine = dangerousAmount($scope.age);
+
+      // calculate minimum caffeine level
+      $scope.minCaffeine = convertLbsToKgs($scope.weight) * 1.5;
+
+      // calculate time till hit min level
+      $scope.doseTime = getTime($scope.curCaffeine);
+
+      // calculate how much caffeine currently in system
+      if($scope.t_0 < 0)
+        $scop.curCaffeine = $scope.consumed;
+      else
+        $scope.curCaffeine = getAmount($scope.consumed, $scope.t_0);      
     }
     
     var init = function() {
-      $scope.showChart = false;
+
     }
     
     init();
